b56171fd7d71604139ac269a5aa6409bd2602b2a
David M. Miller <daxdoxsi@gmail.com>
2021-08-29 15:05:20 -0600

    Add index.php as FE

---
+++ public_html/index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>PODCAST SYSTEM</title>
+    <meta name="copyright" content="Copyright 2009-2021. David Madrigal Miller">
+    <style>
+	html, body {
+		background-color: #dddddd;
+		padding: 0;
+		margin: 0;
+		height: 100%;
+		display: block;
+		font-family: Verdana, Arial;
+		font-size: 16px;
+	} 
+	.wrapper {
+		max-width: 750px;
+		border: solid 2px gray;
+		padding: 50px;
+		margin: 50px auto;
+	} 
+    </style>
+</head>
+<body>
+<div class="wrapper">
+	<h1>Podcast Challenge</h1>
+	<form method="post" action="">
+		<fieldset>
+			<legend>Add new podcast URL</legend>
+			<label for="podcast-url">Type here the Podcast URL: </label>
+			<input type="text" name="podcast_url" size="50" id="podcast-url">
+			<input type="submit" value="Save">
+		</fieldset>
+	</form> 
+</div>
+</body>
+</html>
+
